bfc107d89732f1f297f5144b8902f0f96a8382c6
Luna Rodrigues Machado <lunarodrigues1142@gmail.com>
2023-02-13 21:00:32 -0300

    Cria funcao do dropdown

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 function dropdown() {
     document.getElementById("drophard").classList.toggle("aparecer")
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.botaodrop')) {
+        let dropdown = document.getElementsByClassName("dropskills");
+        let index;
+        for (index = 0; index < dropdown.length; index++) {
+            let abrirDropdown = dropdown[index];
+            if (abrirDropdown.classList.contains("aparecer")) {
+                abrirDropdown.classList.remove("aparecer")
+            }
+        }
+    }
+}
